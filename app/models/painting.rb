2ff14c76e96ff817625b48000c2b58a48133c211
Stacey McKnight <mcknight.stm@gmail.com>
2021-05-05 13:54:32 -0500

    add sort options

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,4 +1,18 @@
 class Painting < ApplicationRecord
   belongs_to :canvas
   has_many :colored_polygons
+
+  scope :newest, -> { order(created_at: :desc) }
+  scope :oldest, -> { order(created_at: :asc) }
+  scope :random, -> { order("RANDOM()")}
+
+  def self.applySort(query)
+    if query === "oldest"
+      Painting.oldest
+    elsif query === "random"
+      Painting.random
+    else
+      Painting.newest
+    end
+  end
 end
