b4fb1e10b2c3682978f49f153d7f565b3590177e
Stacey McKnight <mcknight.stm@gmail.com>
2021-05-06 14:13:08 -0500

    accept nested attributes to make saving simpler

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,7 +1,8 @@
 class Painting < ApplicationRecord
   belongs_to :canvas
   has_many :colored_polygons
-
+  accepts_nested_attributes_for :colored_polygons
+  
   scope :newest, -> { order(created_at: :desc) }
   scope :oldest, -> { order(created_at: :asc) }
   scope :random, -> { order("RANDOM()")}
