f4277463afa683941866999352c013e890aff596
Stacey McKnight <mcknight.stm@gmail.com>
2021-05-06 14:24:33 -0500

    validate color is hex code format

--- app/models/colored_polygon.rb
+++ app/models/colored_polygon.rb
@@ -1,4 +1,6 @@
 class ColoredPolygon < ApplicationRecord
   belongs_to :painting
   belongs_to :polygon
+
+  validates :color, format: { with: /\A#(?:\h{3}){1,2}\z/, message: "must be hex color code" }
 end
